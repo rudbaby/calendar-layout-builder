32474d58d4f6d5ad5894c0ca196871f5ac4e8c9e
Bhanu Koppaka <bkoppaka@ebay.com>
2012-05-18 15:51:07 -0700

    initial version

---
+++ calendar/js/calendar.js
@@ -0,0 +1,60 @@
+/**
+ * Lays out events for a single  day
+ * @param array  events   An array of event objects. Each event object consists of a start and end
+ *                                     time  (measured in minutes) from 9am, as well as a unique id. The
+ *                                     start and end time of each event will be [0, 720]. The start time will
+ *                                     be less than the end time.
+ * @return array  An array of event objects that has the width, the left and top positions set, in addition to the id,
+ *                        start and end time. The object should be laid out so that there are no overlapping
+ *                        events.
+ */
+$(document).ready(function() {
+    var itree = new IntervalTree(360); //center of the tree. which is mid of the calendar.
+
+    function layOutDay(events) {
+        var event, width, layoutWidth = 600, idx = 0;
+        for (i in events) {
+            event = events[i];
+            itree.add([event.start, event.end], idx++);
+        }
+        for (i in events) {
+            event = events[i];
+            if (!event.position) {
+                var overlappedEvents = itree.search(event.start, event.end);
+                if (overlappedEvents.length > 0) {
+                    console.info("overlap\n", JSON.stringify(overlappedEvents));
+                    var counter = 0, width = layoutWidth / overlappedEvents.length;
+                    overlappedEvents.forEach(function(result) {
+                        event = events[result.id];
+                        if (!event.position) {
+                            event.position = {top : event.start, left:width * counter++,width:width};
+                        }
+                    });
+                } else {
+                    event.position = {top : event.start, left:0,width:layoutWidth};
+                }
+            }
+        }
+        return events;
+    }
+
+    var processedEvents = layOutDay([
+        {id : 1, start : 30,  end : 150},// starts at 9:30 am and ends at 11:30 am
+        {id : 2, start : 540, end : 600},// starts at 6:00 pm and ends at 7:00pm
+        {id : 3, start : 560, end : 620},// starts at 6:20pm and ends at 7:20pm
+        {id : 4, start : 610, end : 670} // starts at 7:10pm pm and ends at 8:10 pm
+    ]);
+
+    var eventHtml = $("#event-template").html().trim();
+
+    var eventDashBoard = $(".event-dashboard");
+
+    processedEvents.forEach(function(event) {
+        $(eventHtml).css({
+            top:event.start,
+            left:event.position.left + 10,
+            height:event.end - event.start,
+            width:event.position.width - 5
+        }).appendTo(eventDashBoard);
+    });
+});
